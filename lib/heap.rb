263f2f217f36b9800d2103f442bdd55cd3c2c5f5
Americo <ame.zuz@gmail.com>
2018-05-03 10:21:14 -0700

    Find the parent and child indices

--- lib/heap.rb
+++ lib/heap.rb
@@ -2,6 +2,8 @@ class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    @store = Array.new
+    @prc = prc
   end
 
   def count
@@ -18,9 +20,17 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
+    left_child = parent_index * 2 + 1
+    right_child = parent_index * 2 + 2
+    children = []
+    children.push(left_child) if left_child < len
+    children.push(right_child) if right_child < len
+    children
   end
 
   def self.parent_index(child_index)
+    raise 'root has no parent' if child_index.zero?
+    (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
